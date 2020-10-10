f091c26d07a61ccc166421d2e0e9a47b549bee1b
Andrea Goldstein <angoldst@berkeley.edu>
2020-10-10 11:01:39 -0700

    Realized may not be the best to have study config tracked so removed it from git tracking

--- study_config.R
+++
@@ -1,8 +0,0 @@
-subject = 'COVID014'
-
-
-diary_name <- './COVID19PandemicTeleh-SleepDiaries_DATA_2020-10-09_2244.csv'
-path_to_data <- '~/Box/CoPsyN Sleep Lab/COVID_19 Study/Part 2/Treatment Diaries/'
-
-
-session_order <- c('t1', 't2', 't3', 't4', 'etx', 'st') #sessions of interest
