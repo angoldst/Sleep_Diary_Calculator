9c39833b261d16f26cf10c12086d041db9e34561
Andrea Goldstein <angoldst@berkeley.edu>
2020-10-16 14:01:43 -0700

    Looks like way more changed than actually did. This really only changes pulling session_numeric from a dictionary defined in the study config (to make easier generalization) and a series of small changes to work more easily on a PC.

--- run_report.R
+++ run_report.R
@@ -1,17 +1,17 @@
 library(fs)
 
 source('study_config.R')
-source('~/Documents/redcap_tokens/covid19_token.R')
 source(here::here('R', 'pull_redcap_Report.R'))
 
 #load IDs to complete
-record_ids <- read.csv(path_to_record_ids, header = T, stringsAsFactors = F) %>% pull(record_id) 
+record_ids <- read.csv(path_to_record_ids, header = T, stringsAsFactors = F) 
+record_ids <- record_ids$record_id
 
 for(record in record_ids){
 save(record, file = 'cur_sub.Rdata')
     
   if(!file.exists(path(path_to_data, record))){
-    dir.create(path(path_to_data, record))
+    dir.create(path(path_to_data , record))
   }
   
   rmarkdown::render("Generate Report From Full Data.Rmd",output_file=path(path_to_data, record, paste0(record, '_',Sys.Date(),'.html')))
